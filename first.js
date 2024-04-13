c37b7077c9f2bc1fc8e6db7a405a9fede1ec941c
vishakha <vishakhaprajapati516@gmail.com>
2024-04-13 11:02:05 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,41 @@
+/*var a=15;
+var b=6;
+var c= a+b;
+
+document.write("The value of C is \n",c);
+
+document.write("The value of B is \n",b);
+
+
+document.write("The value of A is \n",a);
+{
+let v;
+v="vishakha Prajapati";
+document.write(v);
+}
+
+let v="vish "
+document.write(v);
+
+const x=5,y =7;
+const z=x+y;
+document.write(z);
+
+/*primitive data type-number ,bolean,string*/
+
+var x=19;
+var y="hello world";
+var flag=false;
+
+var p;
+
+document.write(typeof(x));
+document.write(typeof(y));
+document.write(typeof(flag));
+document.write(typeof(p));
+
+var num=null;
+document.write(typeof(num))
+
+
+
